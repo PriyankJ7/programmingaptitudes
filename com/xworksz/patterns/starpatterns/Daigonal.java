f260b095f65a0fa178952fa176d3c19865564401
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-12-11 19:39:19 +0530

    Star-Patterns

---
+++ com/xworksz/patterns/starpatterns/Daigonal.java
@@ -0,0 +1,26 @@
+package com.xworksz.patterns.starpatterns;
+
+public class Daigonal {
+
+	public static void main(String[] args) {
+		System.out.println("daigonal");
+		
+		int num=5;
+		for(int i=0;i<num;i++)
+		{
+			for(int j=0;j<num;j++)
+			{
+				if(i==j)
+				{
+					System.out.println("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}System.out.println();
+		}
+
+	}
+
+}
